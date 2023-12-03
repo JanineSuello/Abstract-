b1fa00e362b949a776759646e395ba450219d785
JanineSuello <janinegrasyanasuello@gmail.com>
2023-12-03 08:26:44 +0800

    added initial file

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+         Dog babyP = new Dog("BabyPup");
+         Cat babyK = new Cat("BabyKit");
+ 
+        
+        babyP.Color();
+        babyK.Color();
+    }
+
+}
